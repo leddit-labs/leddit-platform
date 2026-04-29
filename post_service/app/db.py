085be9044d613d439d278588a022f48690fcc54d
Nademtis <mathias.99.madsen@gmail.com>
2026-04-29 10:45:55 +0200

    fixed attributeError

--- post_service/app/db.py
+++ post_service/app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
