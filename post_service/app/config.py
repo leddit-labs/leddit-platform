2f7c4a1b35b5cb0cb42f9d7dca91f36c6c76d5f3
Dan Joergensen <danvj93@gmail.com>
2026-04-29 10:27:13 +0200

    Add pydantic settings. Fully replaced dotenv lib with pydantic settings

--- post_service/app/config.py
+++ post_service/app/config.py
@@ -1,6 +1,16 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+class Settings(BaseSettings):
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+
+    database_url: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+settings = Settings()
--- post_service/app/db.py
+++ post_service/app/db.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config import DATABASE_URL
+from app.config import settings
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
