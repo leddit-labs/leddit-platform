14db576a4d855bb545da50b0b7ef9238be873771
Nademtis <mathias.99.madsen@gmail.com>
2026-05-02 16:56:27 +0200

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Container, Heading } from "@chakra-ui/react"
+
+function App() {
+	return (
+		<Container centerContent>
+			<Heading mb={4}>Leddit the platform deluxe woop</Heading>
+			<Button colorScheme="teal">Click here lmao xd</Button>
+		</Container>
+	)
+}
+
+export default App
