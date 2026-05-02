f8f4e20e134e32a6fb8f990f18b4ae876e607897
Nademtis <mathias.99.madsen@gmail.com>
2026-05-02 17:29:53 +0200

    theme fixes

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,13 +1,18 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
-	colors: {
-		brand: {
-			500: "#ffffff",
-			600: "#aaaaaa",
-			700: "#5a5a5a",
-		},
-	},
-})
-console.log("THEME LOADED:", theme)
-console.log("THEME LOADED:", JSON.stringify(theme, null, 2))
+  colors: {
+    brand: {
+      500: "#ffffff",
+      600: "#aaaaaa",
+      700: "#5a5a5a",
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.700",
+      },
+    },
+  },
+});
