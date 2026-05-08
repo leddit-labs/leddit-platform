ab1c33f8b40ff601de30172640f846f43b0a6d96
Davijo <danvj93@gmail.com>
2026-05-08 20:20:37 +0200

    Dont take my test-service away

--- services/community-service/app/main.py
+++ services/community-service/app/main.py
@@ -1,11 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
 from app.config import settings
 from app.database import engine, Base
 from app.router import router
 
-Base.metadata.create_all(bind=engine)
 
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#   Base.metadata.create_all(bind=engine)
+#    yield
+
+
+# app = FastAPI(title=settings.app_name, lifespan=lifespan)
 app = FastAPI(title=settings.app_name)
+
 app.include_router(router)
 
 
