cd924669f9ffc45d3c2dbd38dd48ad58718d2f33
Nademtis <mathias.99.madsen@gmail.com>
2026-06-23 15:46:47 +0200

    added graphQL endpoint and fixed windows kubernetes setup

--- services/post-service/app/main.py
+++ services/post-service/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from strawberry.fastapi import GraphQLRouter
+from app.graphql import schema
 from leddit_logging import setup_logging, RequestLoggingMiddleware
 
 from app.db import Base, engine
@@ -10,6 +11,9 @@ Base.metadata.create_all(bind=engine)
 logger = setup_logging("post-service")
 
 app = FastAPI()
+
+graphql_app = GraphQLRouter(schema)
+app.include_router(graphql_app, prefix="/graphql")
 
 app.add_middleware(RequestLoggingMiddleware)
 app.include_router(post_router)
