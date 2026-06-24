89ee951c43df3fc8036eedc1d75cd016df8d8049
Nademtis <mathias.99.madsen@gmail.com>
2026-06-24 16:42:10 +0200

    grapQL fixes and docker fixes

--- services/post-service/app/main.py
+++ services/post-service/app/main.py
@@ -16,7 +16,7 @@ logger = setup_logging("post-service")
 app = FastAPI()
 
 graphql_app = GraphQLRouter(schema)
-app.include_router(graphql_app, prefix="/graphql")
+app.include_router(graphql_app, prefix="/posts/graphql")
 
 app.add_middleware(RequestLoggingMiddleware)
 app.include_router(post_router)
